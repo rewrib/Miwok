c603e860646f0c02587d95cc6c95b97f679d6d1c
rewrib <birwer.ali@gmx.de>
2019-10-18 13:46:50 +0200

    setting custom background color for each category

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,14 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+
+
 public class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(Context context, ArrayList<Word> words) {
+    private int mColorResourceId;
+
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @Override
@@ -41,6 +47,12 @@ public class WordAdapter extends ArrayAdapter<Word> {
         } else {
             imageView.setVisibility(View.GONE);
         }
+
+        // set background color for given list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
+
         return listItemView;
 
     }
